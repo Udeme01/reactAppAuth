55fe1ef71a58beffb31efc2e7981866d9b2d74b6
unknown <TL@ARMP-SUR-IT101.access-armpensions.com>
2024-12-28 14:47:54 +0100

    token expiration implementation

--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -49,6 +49,9 @@ export const action = async ({ request }) => {
   const token = resData.token;
 
   localStorage.setItem("token", token);
+  const expiration = new Date();
+  expiration.setHours(expiration.getHours() + 1);
+  localStorage.setItem("expiration", expiration.toISOString());
 
   // soon manage that token sent from the backend...
   return redirect("/");
